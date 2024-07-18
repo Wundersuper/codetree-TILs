862857e4da34bb13f67ef24758625bdf41b6cd0a
Wundersuper <salsdk08@gmail.com>
2024-07-18 17:32:22 +0900

    Title: 중앙값 구하기, Time: 96ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240718/중앙값 구하기/find-the-median.py
@@ -0,0 +1,16 @@
+n = input()
+a, b, c = int(n.split()[0]), int(n.split()[1]), int(n.split()[2])
+
+if a >= b:
+    if a <= c:
+        print(a)
+    else:
+        if b >= c:
+            print(b)
+        else:
+            print(c)
+else:
+    if a <= c:
+        print(c)
+    else:
+        print(a)
