d21a39a2f1b2036b33c3d75f52fddc0a350cc91c
Wundersuper <salsdk08@gmail.com>
2024-07-18 17:16:22 +0900

    Title: 최댓값 구하기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240718/최댓값 구하기/maximum-value.py
+++ 240718/최댓값 구하기/maximum-value.py
@@ -3,15 +3,13 @@ a = int(n.split()[0])
 b = int(n.split()[1])
 c = int(n.split()[2])
 
-if a > b:
-    if a > c:
+if a >= b:
+    if a >= c:
         print(a)
     else:
         print(c)
-elif b > c:
-    if b > a:
+else:
+    if b >= c:
         print(b)
     else:
-        print(a)
-else:
-    print(c)
+        print(c)
