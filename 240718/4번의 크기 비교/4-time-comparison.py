93ba4b30db46ac98a553feae92960f8565a07104
Wundersuper <salsdk08@gmail.com>
2024-07-18 15:57:03 +0900

    Title: 4번의 크기 비교, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240718/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+n = input()
+b = int(n.split()[0])
+c = int(n.split()[1])
+d = int(n.split()[2])
+e = int(n.split()[3])
+
+print(int(a > b))
+print(int(a > c))
+print(int(a > d))
+print(int(a > e))
