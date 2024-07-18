a510c3cf968b11c5f11de2473facae0ef33442da
Wundersuper <salsdk08@gmail.com>
2024-07-19 00:53:12 +0900

    Title: a/b 출력, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240719/a/b 출력/a-divide-b.py
@@ -0,0 +1,10 @@
+n = input()
+a = int(n.split()[0])
+b = int(n.split()[1])
+
+print(f'{a//b}.', end = "") #일의 자리
+
+for _ in range(20):
+    a *= 10
+    print(a//b, end = "")
+    a %= b #나머지 곱하기 10을 b로 나눔
