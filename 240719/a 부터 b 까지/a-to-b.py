e06f996940c492747c09c0098caa601c948ca405
Wundersuper <salsdk08@gmail.com>
2024-07-19 13:53:05 +0900

    Title: a 부터 b 까지, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240719/a 부터 b 까지/a-to-b.py
+++ 240719/a 부터 b 까지/a-to-b.py
@@ -2,9 +2,9 @@ a, b = map(int, input().split())
 
 print(a, end = " ")
 while a < b or (a * 2 < b) or (a + 3 < b):
-    if a % 2 == 1:
+    if a % 2 == 1 and a < b:
         a *= 2
         print(a, end = " ")
-    elif a % 2 == 0:
+    elif a % 2 == 0 and a < b:
         a += 3
         print(a, end = " ")
