0e6563d3b4f51466b0d1435f7b2238038c5b9a52
Wundersuper <salsdk08@gmail.com>
2024-07-21 18:30:29 +0900

    Title: 규칙을 찾아 출력하기, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240721/규칙을 찾아 출력하기/find-a-rule-and-print.py
+++ 240721/규칙을 찾아 출력하기/find-a-rule-and-print.py
@@ -1,10 +1,12 @@
 n = int(input())
 
 for i in range(n):
-    if i == 0 or i == (n - 1):
-        for j in range(n):
+    for j in range(n):
+        if i == 0 or i == (n - 1) or j == 0 or j == (n-1):
             print('*', end = " ")
-    else:
-        for j in range(i+1):
-            print('*', end =" ")
+        else:
+            if j < i:
+                print('*', end = " ")
+            else:
+                print(' ', end = " ")
     print()
