4116618e91f3a4eb9d1070a08787ec9e910a4562
Wundersuper <salsdk08@gmail.com>
2024-07-22 18:26:28 +0900

    Title: 10개 입력받기, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240722/10개 입력받기/receive-10-inputs.py
@@ -0,0 +1,15 @@
+arr = list(map(int, input().split()))
+
+cnt = 0
+
+for elem in arr:
+    if elem == 0:
+        break
+    cnt += 1
+
+sum_val = 0
+
+for i in range(cnt):
+    sum_val += arr[i]
+
+print(f'{sum_val} {sum_val / cnt:.1f}')
