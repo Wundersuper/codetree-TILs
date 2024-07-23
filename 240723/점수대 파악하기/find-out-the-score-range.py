b0a115087dd1dcdc0fef7dad0dcde4c5966f7712
Wundersuper <salsdk08@gmail.com>
2024-07-23 15:49:06 +0900

    Title: 점수대 파악하기, Time: 168ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240723/점수대 파악하기/find-out-the-score-range.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input().split()))
+
+count_arr = [0 for _ in range(11)]
+
+for elem in arr:
+    if elem == 0:
+        break
+
+    count_arr[elem // 10] += 1
+
+for i in range(10, 0, -1):
+    print(f'{i * 10} - {count_arr[i]}')
