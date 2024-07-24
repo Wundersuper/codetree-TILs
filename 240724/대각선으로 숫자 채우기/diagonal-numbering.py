b0449cd7205aa9ec21b0bf20329ad10d2a3bb1ef
Wundersuper <salsdk08@gmail.com>
2024-07-24 23:54:52 +0900

    Title: 대각선으로 숫자 채우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240724/대각선으로 숫자 채우기/diagonal-numbering.py
@@ -0,0 +1,24 @@
+n, m = tuple(map(int, input().split()))
+
+arr = [
+    [0 for _ in range(m)]
+    for _ in range(n)
+]
+
+cnt = n+m-2
+val = 1
+
+for i in range(cnt+1):
+    a = 0
+    b = icc
+    for j in range(i+1):
+        if (a<n) & (b<m):
+            arr[a][b] = val
+            val+=1
+        a+=1
+        b-=1
+        
+for row in range(n):
+    for elem in range(m):
+        print(arr[row][elem] , end= " ")
+    print()
