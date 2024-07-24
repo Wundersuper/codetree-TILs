78930c16af6cd7698beeb69ad6eb30e1571ac229
Wundersuper <salsdk08@gmail.com>
2024-07-24 16:53:19 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240724/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240724/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -2,14 +2,25 @@ n = int(input())
 
 price = list(map(int, input().split()))
 
-max_cost = 1
-for i in range(n-1, 0, -1):
-    for j in range(i):
-        plus = price[i] - price[j]
-        if max_cost < plus:
-            max_cost = plus
-        
-if max_cost <= 0:
+# 최대 이익: 최솟값 구하고 최솟값 이후 값에서 최대 찾아서 빼기
+
+prev_min_idx = 0
+min_idx = 0
+
+for i in range(prev_min_idx, n):
+    if price[i] < price[min_idx]:
+        min_idx = i
+
+    prev_min_idx = min_idx
+
+max_idx = min_idx
+for j in range(prev_min_idx, n):
+    if price[max_idx] < price[j]:
+        max_idx = j
+
+    prev_min_idx = max_idx
+
+if min_idx == n-1:
     print('0')
 else:
-    print(max_cost)
+    print(price[max_idx] - price[min_idx])
