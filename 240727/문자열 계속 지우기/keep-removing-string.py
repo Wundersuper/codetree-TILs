360144d7450e5bbc8b66c1fef95349e4f4fe990e
Wundersuper <salsdk08@gmail.com>
2024-07-27 22:09:20 +0900

    Title: 문자열 계속 지우기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240727/문자열 계속 지우기/keep-removing-string.py
+++ 240727/문자열 계속 지우기/keep-removing-string.py
@@ -5,13 +5,13 @@ lst_a = list(str_a)
 
 while True:
     if len(str_a) >= len(str_b):
-        idx = ''.join(lst_a).index(str_b)
-        lst_a = lst_a[:idx] + lst_a[idx+2:]
         if str_b not in ''.join(lst_a):
             print(''.join(lst_a))
             break
         else:
+            idx = ''.join(lst_a).index(str_b)
+            lst_a = lst_a[:idx] + lst_a[idx+2:]
             continue
     else:
         print(str_a)
-        continue
+        break
