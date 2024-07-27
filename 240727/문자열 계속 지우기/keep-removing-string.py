4722db9ee13f8186b35c46dcc38ec0b5345548dc
Wundersuper <salsdk08@gmail.com>
2024-07-27 22:04:28 +0900

    Title: 문자열 계속 지우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240727/문자열 계속 지우기/keep-removing-string.py
+++ 240727/문자열 계속 지우기/keep-removing-string.py
@@ -4,10 +4,14 @@ str_b = input()
 lst_a = list(str_a)
 
 while True:
-    idx = ''.join(lst_a).index(str_b)
-    lst_a = lst_a[:idx] + lst_a[idx+2:]
-    if str_b not in ''.join(lst_a):
-        print(''.join(lst_a))
-        break
+    if len(str_a) >= len(str_b):
+        idx = ''.join(lst_a).index(str_b)
+        lst_a = lst_a[:idx] + lst_a[idx+2:]
+        if str_b not in ''.join(lst_a):
+            print(''.join(lst_a))
+            break
+        else:
+            continue
     else:
-        continue
+        print(str_a)
+        break
