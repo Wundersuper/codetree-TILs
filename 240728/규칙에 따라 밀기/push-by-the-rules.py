85b786b395a0dee72536648a713be08778c70fe3
Wundersuper <salsdk08@gmail.com>
2024-07-28 11:14:41 +0900

    Title: 규칙에 따라 밀기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240728/규칙에 따라 밀기/push-by-the-rules.py
@@ -0,0 +1,14 @@
+A = input()
+B = input() #명령 문자열
+
+lst_B = list(B)
+len_B = len(lst_B)
+
+for i in range(len_B):
+    if lst_B == 'L':
+        A = A[1:] + A[0]
+
+    else:
+        A = A[-1] + A[:-1]
+
+print(A)
