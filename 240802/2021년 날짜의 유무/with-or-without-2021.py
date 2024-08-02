ffff87cbca655d5d59ac532fbec2380d5177cd21
Wundersuper <salsdk08@gmail.com>
2024-08-02 15:04:47 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240802/2021년 날짜의 유무/with-or-without-2021.py
+++ 240802/2021년 날짜의 유무/with-or-without-2021.py
@@ -5,8 +5,10 @@ def days(m):
         return 28
     elif m in (4, 6, 9, 11):
         return 30
-    else:
+    elif m in (1, 3, 5, 7, 8, 10, 12):
         return 31
+    else:
+        return print('No')
 
 def find_day(d):
     for i in range(1, days(M)+1):
