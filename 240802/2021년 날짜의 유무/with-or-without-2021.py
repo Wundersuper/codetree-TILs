94820975121b107e41dac0c180636f93e9fda29d
Wundersuper <salsdk08@gmail.com>
2024-08-02 15:06:24 +0900

    Title: 2021년 날짜의 유무, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240802/2021년 날짜의 유무/with-or-without-2021.py
+++ 240802/2021년 날짜의 유무/with-or-without-2021.py
@@ -4,18 +4,17 @@ def days(m):
     if m == 2:
         return 28
     elif m in (4, 6, 9, 11):
-        return 30
-    elif m in (1, 3, 5, 7, 8, 10, 12):
-        return 31
+        return 30 
     else:
-        return print('No')
+        return 31
 
 def find_day(d):
-    for i in range(1, days(M)+1):
-        if i == d:
-            return True
-    
-    return False
+    if M <= 12:
+        for i in range(1, days(M)+1):
+            if i == d:
+                return True
+    else:
+        return False
 
 
 if find_day(D):
