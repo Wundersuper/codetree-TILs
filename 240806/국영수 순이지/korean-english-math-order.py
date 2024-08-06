a3e22d044f31dc72a36d95faad8f1fdb308eff72
Wundersuper <salsdk08@gmail.com>
2024-08-06 16:22:30 +0900

    Title: 국영수 순이지, Time: 133ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240806/국영수 순이지/korean-english-math-order.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+
+n = int(input())
+arr = [tuple(input().split()) for _ in range(n)]
+students = [
+    Student(name, int(kor), int(eng), int(math))
+    for name, kor, eng, math in arr
+    ]
+
+students.sort(key=lambda x: (-x.kor, -x.eng, -x.math))
+
+for s in students:
+    print(s.name, s.kor, s.eng, s.math)
