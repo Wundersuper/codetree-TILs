00a41749fab10e079d1dcde728bf7d081d1f14b3
Wundersuper <salsdk08@gmail.com>
2024-08-08 20:01:56 +0900

    Title: DateTime to DateTime, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240808/DateTime to DateTime/datetime-to-datetime.py
+++ 240808/DateTime to DateTime/datetime-to-datetime.py
@@ -1,6 +1,10 @@
 a, b, c = tuple(map(int, input().split()))
 
-if a < 11 or b < 11 or c < 11:
+if a < 11:
+    print(-1)
+elif b < 11:
+    print(-1)
+elif c < 11:
     print(-1)
 else:
     print((a-11)*24*60 + (b-11)*60 + (c-11))
