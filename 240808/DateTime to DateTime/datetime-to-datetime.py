b6d0104f2bb82aa1a7cde69178be45387c9457a4
Wundersuper <salsdk08@gmail.com>
2024-08-08 20:06:09 +0900

    Title: DateTime to DateTime, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240808/DateTime to DateTime/datetime-to-datetime.py
+++ 240808/DateTime to DateTime/datetime-to-datetime.py
@@ -1,10 +1,13 @@
 a, b, c = tuple(map(int, input().split()))
 
-if a < 11:
-    print(-1)
-elif b < 11:
-    print(-1)
-elif c < 11:
+#11월 11일 11시 11분 - 11월 1일 0시 0분
+time1 = (11-1)*24*60+(11-0)*60+(11-0)
+#a일 b시 c분 - 11월 1일 0시 0분
+time2 = (a-1)*24*60+(b-0)*60+(c-0)
+
+diff = time2 - time1
+
+if diff < 0:
     print(-1)
 else:
-    print((a-11)*24*60 + (b-11)*60 + (c-11))
+    print(diff)
