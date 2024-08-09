29771a58d2c51f6b0348c8a348b85e24bd43c063
Wundersuper <salsdk08@gmail.com>
2024-08-09 22:50:47 +0900

    Title: 왔다 갔던 구역 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240809/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+line = [0 for _ in range(203)]
+start = 101
+
+for i in range(n):
+    x, d = input().split()
+    x = int(x)
+
+    if d == 'L':
+        for j in range(x):
+            line[start - j] += 1
+        start -= x
+    else:
+        for j in range(x):
+            line[start + j] += 1
+        start += x
+
+count = 0
+for i in range(len(line)):
+    if line[i] >= 2:
+        count += 1
+
+print(count)
