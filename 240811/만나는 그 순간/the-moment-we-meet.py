bc59ac79e78ba87e79b9e7b08106242f9481f851
Wundersuper <salsdk08@gmail.com>
2024-08-11 11:41:10 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240811/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,34 @@
+N, M = tuple(map(int, input().split()))
+
+A = []
+B = []
+
+loc_a, loc_b = 0, 0
+for i in range(N):
+    direction, move = tuple(input().split())
+    move = int(move)
+    if direction == 'L':
+        for _ in range(move):
+            loc_a -= 1
+            A.append(loc_a)
+    else:
+        for _ in range(move):
+            loc_a += 1
+            A.append(loc_a)      
+
+for i in range(M):
+    direction, move = tuple(input().split())
+    move = int(move)
+    if direction == 'L':
+        for _ in range(move):
+            loc_b -= 1
+            B.append(loc_b)
+    else:
+        for _ in range(move):
+            loc_b += 1
+            B.append(loc_b)
+
+for i in range(1, 1000000):
+    if A[i] == B[i]:
+        print(i+1)
+        break
