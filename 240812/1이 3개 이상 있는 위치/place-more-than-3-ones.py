cdcde479fe7c439e768e87e9abaa9dceadaebb70
Wundersuper <salsdk08@gmail.com>
2024-08-12 22:18:04 +0900

    Title: 1이 3개 이상 있는 위치, Time: 148ms, Memory: 25MB, Status: Passed - CodeTree

--- 240812/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240812/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -12,19 +12,19 @@ def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
 
-cnt = 0
 cnt_one = 0
 
 
 for i in range(n):
     for j in range(n):
+        cnt = 0
         for dx, dy in zip(dxs, dys):
             x, y = i, j
             nx, ny = x + dx, y + dy
             if in_range(nx, ny) and arr[nx][ny] == 1:
                 cnt += 1
         
-    if cnt >= 3:
-        cnt_one += 1
+        if cnt >= 3:
+            cnt_one += 1
 
 print(cnt_one)
