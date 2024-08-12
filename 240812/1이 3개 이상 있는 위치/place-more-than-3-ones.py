0cb8787e4462c879083ca0edf9c285bb4319c5b5
Wundersuper <salsdk08@gmail.com>
2024-08-12 22:15:17 +0900

    Title: 1이 3개 이상 있는 위치, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240812/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+dxs, dys = [-1, 0, 1, 0], [0, 1, 0, -1] #북동남서 0123
+
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+cnt = 0
+cnt_one = 0
+
+for dx, dy in zip(dxs, dys):
+    for i in range(n):
+        for j in range(n):
+            x, y = i, j
+            nx, ny = x + dx, y + dy
+            if in_range(nx, ny) and arr[nx][ny] == 1:
+                cnt += 1
+        
+    if cnt >= 3:
+        cnt_one += 1
+
+print(cnt_one)
