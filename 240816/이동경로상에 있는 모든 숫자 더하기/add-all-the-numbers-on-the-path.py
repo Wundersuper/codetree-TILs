f1549ace50db9abb61be01afe168171b2f3b1d84
Wundersuper <salsdk08@gmail.com>
2024-08-16 13:33:34 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 159ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240816/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -0,0 +1,30 @@
+N, T = tuple(map(int, input().split()))
+cmd = list(input())
+
+tile = [
+    list(map(int, input().split()))
+    for _ in range(N)
+]
+
+
+def in_range(x, y):
+    return 0 <= x and x < N and 0 <= y and y < N
+
+
+dxs, dys = [-1, 0, 1, 0], [0, -1, 0, 1] #ULDR
+x, y, dir_num = N//2, N//2, 0
+
+sum_val = tile[x][y]
+for elem in cmd:
+    if elem == 'L':
+        dir_num = (dir_num + 1) % 4
+    elif elem == 'R':
+        dir_num = (dir_num + 4 - 1) % 4
+    elif elem == 'F':
+        x, y = x + dxs[dir_num], y + dys[dir_num]
+    if not in_range(x, y):
+        continue
+    
+    sum_val += tile[x][y]
+
+print(sum_val)
