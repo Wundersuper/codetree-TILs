79e564398aaece4ed66ce9571588963ca3eb0374
Wundersuper <salsdk08@gmail.com>
2024-08-16 13:17:09 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240816/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 240816/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -2,18 +2,18 @@ n = int(input()) #항상 홀수
 
 tile = [[0] * n for _ in range(n)]
 
-dxs, dys = [0, -1, 0, 1], [1, 0, -1, 0] #RULD
-x, y, dir_num = (n-1)//2, (n-1)//2, 0
-tile[x][y] = 1
+dxs, dys = [0, -1, 0, 1], [-1, 0, 1, 0] #LURD
+x, y, dir_num = n-1, n-1, 0
+tile[x][y] = n * n
 
 
 def in_range(x, y):
     return 0 <= x and x < n and 0 <= y and y < n
 
 
-for i in range(2, n*n+1):
+for i in range(n*n-1, 0, -1):
     nx, ny = x + dxs[dir_num], y + dys[dir_num]
-    if not in_range(nx, ny):
+    if not in_range(nx, ny) or tile[nx][ny] != 0:
         dir_num = (dir_num + 1) % 4
     
     x, y = x + dxs[dir_num], y + dys[dir_num]
