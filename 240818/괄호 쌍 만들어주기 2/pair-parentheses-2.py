f677544f312a60aa3e41e1ef44c7508ebada93c7
Wundersuper <salsdk08@gmail.com>
2024-08-18 20:00:22 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 240818/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 240818/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,9 +1,9 @@
 A = list(input())
 
 ans = 0
-for i in range(len(A)-2):
-    for j in range(i+2, len(A)-2):
-        if A[i] + A[i+1] == '((' and A[j] + A[j+2] == '))':
+for i in range(len(A)-3):
+    for j in range(i+2, len(A)-1):
+        if A[i] + A[i+1] == '((' and A[j] + A[j+1] == '))':
             ans += 1
 
 print(ans)
