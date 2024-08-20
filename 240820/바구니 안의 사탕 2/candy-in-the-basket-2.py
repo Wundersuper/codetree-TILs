7f38709642218400e372706b7b81d8f4ec60ea5d
Wundersuper <salsdk08@gmail.com>
2024-08-20 21:34:56 +0900

    Title: 바구니 안의 사탕 2, Time: 161ms, Memory: 25MB, Status: Passed - CodeTree

--- 240820/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 240820/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,14 +1,14 @@
 import sys
 
 N, K = tuple(map(int, input().split()))
-basket = [0] * 102
+basket = [0] * 10000
 
 for i in range(N):
     candy, loc = tuple(map(int, input().split()))
     basket[loc] += candy
 
 max_candy = -sys.maxsize
-for i in range(102 - 2 * K):
+for i in range(10000 - 2 * K):
     cnt = 0
     for j in range(i, i + 2 * K + 1):
         cnt += basket[j]
