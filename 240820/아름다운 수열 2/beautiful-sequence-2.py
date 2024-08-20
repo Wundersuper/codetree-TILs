ca0862544ce02b8ba8a4e70daa2293095584f249
Wundersuper <salsdk08@gmail.com>
2024-08-20 21:14:07 +0900

    Title: 아름다운 수열 2, Time: 159ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240820/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,14 @@
+N, M = tuple(map(int, input().split()))
+arr_a = list(map(int, input().split())) # N
+arr_b = list(map(int, input().split())) # M
+
+cnt = 0
+for i in range(N-2):
+    part_a = []
+    for j in range(i, i+3):
+        part_a.append(arr_a[j])
+    
+    if sorted(part_a) == sorted(arr_b):
+        cnt += 1
+
+print(cnt)
