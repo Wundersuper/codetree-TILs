224f405e89b9788e0f36bdadf643ce7926462397
Wundersuper <salsdk08@gmail.com>
2024-08-22 18:11:57 +0900

    Title: 개발팀의 능력, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240822/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240822/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -8,13 +8,14 @@ min_diff = sys.maxsize
 for i in range(5):
     for j in range(i+1, 5):
         for k in range(5):
-            team_b = arr[i] + arr[j]
-            team_c = arr[k]
-            team_a = total - team_c - team_b
-            if team_a != team_b and team_b != team_c and team_c != team_a:
-                is_possible = True
-                diff = max(team_a, team_b, team_c) - min(team_a, team_b, team_c)
-                min_diff = min(min_diff, diff)
+            if k != i and k != j:
+                team_b = arr[i] + arr[j]
+                team_c = arr[k]
+                team_a = total - team_c - team_b
+                if team_a != team_b and team_b != team_c and team_c != team_a:
+                    is_possible = True
+                    diff = max(team_a, team_b, team_c) - min(team_a, team_b, team_c)
+                    min_diff = min(min_diff, diff)
 
 if is_possible:
     print(min_diff)
