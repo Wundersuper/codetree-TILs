59a301bf319271a69e417f2460da7e040c8df4fc
Wundersuper <salsdk08@gmail.com>
2024-08-22 18:11:15 +0900

    Title: 개발팀의 능력, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240822/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 240822/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -1,32 +1,22 @@
 import sys
 
-score = list(map(int, input().split()))
+arr = list(map(int, input().split()))
+total = sum(arr)
+is_possible = False
+min_diff = sys.maxsize
 
-
-def get_diff(a, b, c, d):
-    sum1 = score[a] + score[b]
-    sum2 = score[c] + score[d]
-    sum3 = sum(score) - (sum1 + sum2)
-    
-    min_diff = min(sum1, sum2, sum3)
-    max_diff = max(sum1, sum2, sum3)
-
-    result = 0
-    if sum1 != sum2 and sum2 != sum3 and sum3 != sum1:
-        return max_diff - min_diff
-    elif sum1 == sum2 or sum2 == sum3 or sum3 == sum1:
-        return -1
-
-
-min_val = sys.maxsize
 for i in range(5):
     for j in range(i+1, 5):
         for k in range(5):
-            for l in range(k+1, 5):
-                if k == i or k == j or l == i or l == j:
-                    continue
-                if get_diff(i, j, k, l) == -1:
-                    continue
-                min_val = min(min_val, get_diff(i, j, k, l))
+            team_b = arr[i] + arr[j]
+            team_c = arr[k]
+            team_a = total - team_c - team_b
+            if team_a != team_b and team_b != team_c and team_c != team_a:
+                is_possible = True
+                diff = max(team_a, team_b, team_c) - min(team_a, team_b, team_c)
+                min_diff = min(min_diff, diff)
 
-print(min_val)
+if is_possible:
+    print(min_diff)
+else:
+    print(-1)
