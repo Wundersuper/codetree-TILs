ae974c1b618c500541bd10e2e72a4b4823ac36e2
Wundersuper <salsdk08@gmail.com>
2024-08-28 15:07:56 +0900

    Title: 야바위, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 240828/야바위/ya-rock.py
+++ 240828/야바위/ya-rock.py
@@ -7,9 +7,10 @@ arr = [
 ans = 0
 for i in range(1, 4): # 종이컵 1,2,3
     stone = [0] * 4
+    stone[i] += 1
+
     cnt = 0
     for j in range(n):
-        stone[i] += 1
         a, b, c = arr[j]
         if stone[a] == 1:
             stone[a] -= 1
